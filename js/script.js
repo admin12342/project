088ec834273e1887cc54ba1e3abc172843d4fc43
ivan <example@gmail.com>
2023-11-23 22:45:49 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use sctrict";
+
+
+// const result=confirm("are you here");
+// console.log(result);
+
+// const answer=+prompt("Skolka let?","18");
+// console.log(answer+5);
+const answers=[];
+answers[0]=prompt('Your name?','');
+answers[1]=prompt('Your age?','');
+answers[2]=prompt('Your nationality?','');
+
+console.log(typeof(answers));
+console.log(typeof(null));
+
